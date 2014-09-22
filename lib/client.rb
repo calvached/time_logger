cae2611ffb8d530d731bfae405daa0ad74a3dd7f
calvached <calvache.diana@gmail.com>
2014-09-21 23:27:03 -0500

    Setting up Client

---
+++ lib/client.rb
@@ -0,0 +1,7 @@
+class Client
+  attr_reader :company_name
+
+  def initialize(company_name)
+    @company_name = company_name
+  end
+end
---
+++ spec/client_spec.rb
@@ -0,0 +1,4 @@
+require 'client'
+
+describe Client do
+end
