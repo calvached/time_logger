a4a38d56e29f1f66ac3bcfc8b8c05016bbdaf4ae
calvached <calvache.diana@gmail.com>
2014-09-23 11:52:28 -0500

    Adding billable projects

---
+++ lib/billable.rb
@@ -0,0 +1,10 @@
+class Billable
+  def initialize(year, month, day, hours, username, client)
+    @year = year
+    @month = month
+    @day = day
+    @hours = hours
+    @username = username
+    @client = client
+  end
+end
---
+++ spec/billable_spec.rb
@@ -0,0 +1,4 @@
+require 'billable'
+
+describe Billable do
+end
