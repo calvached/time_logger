072b8783ceefd08140e9d8fca85dc6f5a24a70e1
calvached <calvache.diana@gmail.com>
2014-09-23 11:37:26 -0500

    Refactoring and adding logic for different projects

--- lib/database.rb
+++ lib/database.rb
@@ -4,31 +4,32 @@ require 'admin'
 require 'client'
 
 class Database
-  attr_reader :employees_file, :clients_file
+  attr_reader :users_file, :clients_file, :pto_file, :billable_file, :non_billable_file
 
   def initialize(files)
-    @employees_file = files.fetch(:employees_file)
+    @users_file = files.fetch(:users_file)
     @clients_file = files.fetch(:clients_file)
+    @pto_file = files.fetch(:pto_file)
+    @billable_file = files.fetch(:billable_file)
+    @non_billable_file = files.fetch(:non_billable_file)
   end
 
   def find(username)
     all_users.select { |user| user.username == username }[0]
   end
 
-  def create_user(username, admin_status)
+  def add_user(username, admin_status)
     File.open(employees_file, 'a') { |file| file.write("#{username},#{admin_status}") }
   end
 
   def admins
-    found_admins.reduce([]) do |admin_list, admin|
-      admin_list << Admin.new(admin[:username])
-    end
+    found_admins = found_user_type('admin')
+    create_user_type(Admin, found_admins)
   end
 
   def employees
-    found_employees.reduce([]) do |employee_list, employee|
-      employee_list << Employee.new(employee[:username])
-    end
+    found_employees = found_user_type('employee')
+    create_user_type(Employee, found_employees)
   end
 
   def all_users
@@ -41,16 +42,50 @@ class Database
     end
   end
 
-  private
-  def found_admins
-    parsed(employees_file).select { |employee| employee[:admin_status] }
+  def all_pto
+    create_project_type(Pto, parsed(pto_file))
   end
 
-  def found_employees
-    parsed(employees_file).select { |employee| !employee[:admin_status] }
+  def all_non_billable
+    create_project_type(NonBillable, parsed(non_billable_file))
+  end
+
+  def all_billable
+    parsed(billable_file).reduce([]) do |collection, row|
+      collection << Billable.new(row[:year],
+                                   row[:month],
+                                   row[:day],
+                                   row[:hours],
+                                   row[:client],
+                                   row[:username])
+    end
+  end
+
+  # DB Users, Clients, Timecodes(Projects)
+
+  private
+  def found_user_type(user_type)
+    parsed(users_file).select { |user| user[:status_code] == user_type }
   end
 
   def parsed(file)
     Parser.parse(file)
+  end
+
+  def create_user_type(class_name, parsed_file)
+    parsed_file.reduce([]) do |collection, row|
+      collection << class_name.new(row[:username],
+                                   row[:status_code])
+    end
+  end
+
+  def create_project_type(class_name, parsed_file)
+    parsed_file.reduce([]) do |collection, row|
+      collection << class_name.new(row[:year],
+                                   row[:month],
+                                   row[:day],
+                                   row[:hours],
+                                   row[:username])
+    end
   end
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,47 +2,72 @@ require 'database'
 
 describe Database do
   let(:files) {{
-                employees_file: 'data/test_employees.csv',
+                users_file: 'data/test_employees.csv',
                 clients_file: 'data/test_clients.csv',
+                pto_file: 'data/test_pto.csv',
+                billable_file: 'data/test_billable.csv',
+                non_billable_file: 'data/test_non_billable.csv',
                }}
 
   let(:database) { Database.new(files) }
 
-  it 'returns all users' do
-    expect(database.all_users).to include(Admin)
-    expect(database.all_users).to include(Employee)
+  describe 'Users' do
+    it 'returns all users' do
+      expect(database.all_users).to include(Admin)
+      expect(database.all_users).to include(Employee)
+    end
+
+    it 'returns all admins' do
+      only_admins = database.admins.all? { |admin| admin.class }
+
+      expect(only_admins).to eq(true)
+    end
+
+    it 'returns all employees' do
+      only_employees = database.employees.all? { |employee| employee.class }
+
+      expect(only_employees).to eq(true)
+    end
+
+    it 'returns all clients' do
+      only_clients = database.clients.all? { |client| client.class }
+
+      expect(only_clients).to eq(true)
+    end
+
+    it 'returns an employee' do
+      expect(database.find('carlsagan')).to be_a_kind_of(Employee)
+    end
+
+    it 'returns an admin' do
+      expect(database.find('calvached')).to be_a_kind_of(Admin)
+    end
+
+    xit 'adds a new employee' do
+      # need to figure out how to test writing to a file
+      database.add_user('flufferkins', 'admin')
+
+      expect(database.find('flufferkins')).to_not be_nil
+    end
   end
 
-  it 'returns all admins' do
-    only_admins = database.admins.all? { |admin| admin.class }
+  describe 'Time Codes' do
+    it 'returns all pto projects' do
+      ptos = database.all_pto.all? { |pto| pto.class }
 
-    expect(only_admins).to eq(true)
-  end
+      expect(ptos).to eq(true)
+    end
 
-  it 'returns all employees' do
-    only_employees = database.employees.all? { |employee| employee.class }
+    it 'returns all billable projects' do
+      bills = database.all_billable.all? { |bill| bill.class }
 
-    expect(only_employees).to eq(true)
-  end
+      expect(bills).to eq(true)
+    end
 
-  it 'returns all clients' do
-    only_clients = database.clients.all? { |client| client.class }
+    it 'returns all non billable projects' do
+      non_bills = database.all_non_billable.all? { |non_bill| non_bill.class }
 
-    expect(only_clients).to eq(true)
-  end
-
-  it 'returns an employee' do
-    expect(database.find('carlsagan')).to be_a_kind_of(Employee)
-  end
-
-  it 'returns an admin' do
-    expect(database.find('calvached')).to be_a_kind_of(Admin)
-  end
-
-  xit 'adds a new employee' do
-    # need to figure out how to test writing to a file
-    database.create_user('flufferkins', 'false')
-
-    expect(database.find('flufferkins')).to_not be_nil
+      expect(non_bills).to eq(true)
+    end
   end
 end
