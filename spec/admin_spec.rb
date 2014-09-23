3b2915acfa1dd61ef6285fd4a4de307d2f4eed41
calvached <calvache.diana@gmail.com>
2014-09-23 11:43:59 -0500

    Adding tests

--- spec/admin_spec.rb
+++ spec/admin_spec.rb
@@ -1,4 +1,13 @@
 require 'admin'
 
 describe Admin do
+  let (:admin) { Admin.new('cupcakegalore', 'employee') }
+
+  it 'contains a username' do
+    expect(admin.username).to eq('cupcakegalore')
+  end
+
+  it 'has a status code' do
+    expect(admin.status_code).to eq('employee')
+  end
 end
