f43fcc5a6ec91976cdedbb4abfb4d54e4f4a1d04
calvached <calvache.diana@gmail.com>
2014-09-23 11:41:27 -0500

    Refactoring Parser to accept projects

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,18 +5,9 @@ class Parser
     parsed_data = []
 
     CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
-      if row[:admin_status]
-        row[:admin_status] = to_boolean(row[:admin_status])
-      end
-
       parsed_data << row
     end
 
     parsed_data
-  end
-
-  private
-  def self.to_boolean(string)
-    string == 'true'
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,9 +5,10 @@ describe Parser do
     employees = Parser.parse('data/test_employees.csv')
 
     expect(employees.first[:username]).to eq('carlsagan')
-    expect(employees.first[:admin_status]).to eq(false)
+    expect(employees.first[:status_code]).to eq('employee')
+
     expect(employees.last[:username]).to eq('calvached')
-    expect(employees.last[:admin_status]).to eq(true)
+    expect(employees.last[:status_code]).to eq('admin')
   end
 
   it 'parses a file of clients' do
@@ -15,5 +16,36 @@ describe Parser do
 
     expect(clients.first[:company_name]).to eq('Fluffy Couches')
     expect(clients.last[:company_name]).to eq('Subway')
+  end
+
+  it 'parses billable projects' do
+    billable = Parser.parse('data/test_billable.csv')
+
+    expect(billable.first[:username]).to eq('calvached')
+    expect(billable.first[:client]).to eq('Fluffy Couches')
+    expect(billable.first[:hours]).to eq('2')
+    expect(billable.first[:year]).to eq('2014')
+    expect(billable.first[:month]).to eq('07')
+    expect(billable.first[:day]).to eq('01')
+  end
+
+  it 'parses non billable projects' do
+    billable = Parser.parse('data/test_non_billable.csv')
+
+    expect(billable.first[:username]).to eq('annerice')
+    expect(billable.first[:hours]).to eq('3')
+    expect(billable.first[:year]).to eq('2014')
+    expect(billable.first[:month]).to eq('08')
+    expect(billable.first[:day]).to eq('20')
+  end
+
+  it 'parses pto projects' do
+    billable = Parser.parse('data/test_pto.csv')
+
+    expect(billable.first[:username]).to eq('alexanderhamilton')
+    expect(billable.first[:hours]).to eq('3')
+    expect(billable.first[:year]).to eq('2014')
+    expect(billable.first[:month]).to eq('08')
+    expect(billable.first[:day]).to eq('23')
   end
 end
