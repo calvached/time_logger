b9f64f524646db51c875a57e14d3275debd45963
calvached <calvache.diana@gmail.com>
2014-09-23 11:53:14 -0500

    Adding pto timecode

---
+++ lib/pto.rb
@@ -0,0 +1,9 @@
+class Pto
+  def initialize(year, month, day, hours, username)
+    @year = year
+    @month = month
+    @day = day
+    @hours = hours
+    @username = username
+  end
+end
---
+++ spec/pto_spec.rb
@@ -0,0 +1,4 @@
+require 'pto'
+
+describe Pto do
+end
